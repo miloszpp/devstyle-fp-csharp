60349ac49dade351466ab9992911749322d2b5ac
Miłosz Piechocki <miloszpp@gmail.com>
2018-04-28 17:56:17 +0200

    Initial commit

---
+++ FP.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace devstylefpcsharp
+{
+    public static class FP
+    {
+        public static Func<T1, Func<T2, R>> Curry<T1, T2, R>(Func<T1, T2, R> func)
+        {
+            return x => y => func(x, y);
+        }
+
+        public static Func<T1, Func<T2, Func<T3, R>>> Curry<T1, T2, T3, R>(Func<T1, T2, T3, R> func)
+        {
+            return x => y => z => func(x, y, z);
+        }
+
+        public static Func<T1, T3> Compose<T1, T2, T3>(Func<T1, T2> f1, Func<T2, T3> f2) 
+        {
+            return x => f2(f1(x));
+        }
+
+        public static Func<T1, T4> Compose<T1, T2, T3, T4>(Func<T1, T2> f1, Func<T2, T3> f2, Func<T3, T4> f3)
+        {
+            return x => f3(f2(f1(x)));
+        }
+
+        public static Func<T1, T5> Compose<T1, T2, T3, T4, T5>(Func<T1, T2> f1, Func<T2, T3> f2, Func<T3, T4> f3, Func<T4, T5> f4)
+        {
+            return x => f4(f3(f2(f1(x))));
+        }
+
+        public static T Reduce<T>(Func<T, T, T> reducer, IEnumerable<T> items) 
+        {
+            return items.Aggregate(reducer);
+        }
+
+        public static Func<IEnumerable<T>, T> Reduce<T>(Func<T, T, T> reducer)
+        {
+            return items => items.Aggregate(reducer);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Reflection.Metadata.Ecma335;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace devstylefpcsharp
+{
+    using static FP;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string> normalizeSymbol = symbol => symbol.ToLower();
+
+            var fetchChart = Curry<string, string, string>((period, symbol) =>
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    return httpClient.GetStringAsync($"https://api.iextrading.com/1.0/stock/{symbol}/chart/{period}").Result;
+                }
+            });
+
+            Func<string, ChartItem> getChartItems = Compose(
+                normalizeSymbol, 
+                fetchChart("1m"),
+                JsonConvert.DeserializeObject<ChartItem[]>,
+                Reduce<ChartItem>((candidate, item) => candidate.Close > item.Close ? candidate : item)
+            );
+
+            //Func<string, ChartItem> getChartItems = Compose(
+            //    (Func<string, string>)Normalize, 
+            //    Curry<string, string, string>(Fetch)("1m"),
+            //    Parse,
+            //    Reduce<ChartItem>((candidate, item) => candidate.Close > item.Close ? candidate : item)
+            //);
+
+            var max = getChartItems("AAPL");
+        }
+
+        static string Normalize(string symbol)
+            => symbol.ToLower();
+
+        static string Fetch(string period, string symbol) 
+        {
+            using (var httpClient = new HttpClient())
+            {
+                return httpClient.GetStringAsync($"https://api.iextrading.com/1.0/stock/{symbol}/chart/{period}").Result;
+            }    
+        }
+
+        static ChartItem[] Parse(string json) 
+            => JsonConvert.DeserializeObject<ChartItem[]>(json);
+
+
+    }
+
+    class ChartItem {
+        public DateTime Date { get; set; }
+        public double Close { get; set; }
+    }
+}
